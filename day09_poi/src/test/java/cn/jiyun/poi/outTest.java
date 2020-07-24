454b0c7f66fc6168a9e22e59966f06b3d403ffdc
SunRuoh <2949133650@qq.com>
2020-07-24 09:21:18 +0800

    代码

---
+++ day09_poi/src/test/java/cn/jiyun/poi/outTest.java
@@ -0,0 +1,35 @@
+package cn.jiyun.poi;
+
+import java.io.FileOutputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class outTest {
+
+	
+	public void out() throws Exception{
+
+		//1.实例化工作簿
+		XSSFWorkbook workbook = new XSSFWorkbook();
+		//2.创建sheet页
+		XSSFSheet sheet = workbook.createSheet("学生信息");
+		//3.创建行row
+		XSSFRow row0 = sheet.createRow(0);
+		//4.创建单元格并赋值
+		String [] title={"编号","姓名","性别","爱好","班级"};
+		for (int i = 0; i < title.length; i++) {
+			XSSFCell cell = row0.createCell(0);
+			cell.setCellValue(title[i]);
+		}
+	
+		//5.导出
+		FileOutputStream outputStream = new FileOutputStream("D:/学生信息.xlsx");
+		workbook.write(outputStream);
+	
+	}
+	
+	
+}
